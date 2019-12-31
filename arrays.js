303fce2b417c9b87e4e6afb8a97df82676f92b9c
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 05:03:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"]
 
 function addElementToBeginningOfArray(array, element)
 {
-  [element, ...array] // non destructive
+  var result = [element, ...array] // non destructive
 }
 
